2d4c395a568f15360343eb89c0b2daaa91c6d9ab
anji <romananji191@gmail.com>
2024-07-29 19:58:30 +0530

    anji

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ app.listen(3000,()=>{
     console.log(`app is listening to the server http://localhost:${3000}`)
 });
 app.get("/",(request,response)=>{
-    response.send("hello from backend,good evening")
+    response.send("hello from the backend,good evening")
 })
 module.exports=app;
