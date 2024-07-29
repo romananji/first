0ce7872b46f26b1fd9a174d5e2802cd9b226d606
anji <romananji191@gmail.com>
2024-07-29 19:44:41 +0530

    adding file

---
+++ index.js
@@ -0,0 +1,9 @@
+const express=require("express");
+const app=express();
+app.listen(3000,()=>{
+    console.log(`app is listening to the server http://localhost:${3000}`)
+});
+app.get("/",(request,response)=>{
+    response.send("hello from backend,good evening")
+})
+module.exports=app;
